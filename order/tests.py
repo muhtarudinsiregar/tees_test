3e94074c2eaa9595834825a05de771e8a96a4494
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-12-16 14:10:25 +0700

    Setup JWT + test auth login

--- order/tests.py
+++ order/tests.py
@@ -1,17 +1,41 @@
 import json
 
-from django.test import Client, TestCase
+from rest_framework.test import APITestCase, APIClient
 from django.urls import reverse
 from rest_framework import status
-
+from order.models import Order
 from order.serializers import OrderSerializer
-
-
-client = Client()
+from django.contrib.auth.models import User
 # Create your tests here.
 
 
-class OrdersTest(TestCase):
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    def login(self, username="", password=""):
+        url = reverse(
+            "create-token"
+        )
+        return self.client.post(
+            url,
+            data=json.dumps({
+                "username": username,
+                "password": password
+            }),
+            content_type="application/json"
+        )
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="admin",
+            email="admin@admin.com",
+            password="superadmin",
+            first_name="test",
+            last_name="user",
+        )
+
+
+class GetAllOrdersTest(BaseViewTest):
     def setUp(self):
         self.valid_payload = {
             'name': 'Muffin',
@@ -21,13 +45,53 @@ class OrdersTest(TestCase):
         }
 
     def test_get_all_orders(self):
-        response = client.get(reverse('get_all_order'))
-        self.assertEqual(response.status_code, status)
+        # fetch the data from db
+        expected = Order.objects.all()
+        serialized = OrderSerializer(expected, many=True)
 
-    def test_store_order(self):
-        response = client.post(
-            reverse('get_all_order'),
-            data=json.dumps(self.valid_payload),
-            content_type='application/json'
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # hit api
+        response = client.get(reverse('orders-all', kwargs={"version": "v1"}))
+
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetDetailOrderTest(BaseViewTest):
+    def test_get_detail_order(self):
+        expected = Order.objects.get(pk=1)
+        serialized = OrderSerializer(expected, many=True)
+
+        response = client.get(reverse('get_detail_order'))
+
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+# class DeleteOrderTest(TestCase):
+#     def test_delete_order(self):
+#         response = client.delete(reverse('get_detail_order'))
+
+#         # self.assertEqual(response.data, serialized.data)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+# class CreateOrderTest(TestCase):
+#     def test_create_order(self):
+#         response = client.post(reverse('get_detail_order'))
+
+#         # self.assertEqual(response.data, serialized.data)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+# class UpdateOrderTest(TestCase):
+#     def test_create_order(self):
+#         response = client.put(reverse('get_detail_order'))
+
+#         # self.assertEqual(response.data, serialized.data)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class AuthLoginTest(BaseViewTest):
+    def test_valid_login_user(self):
+        response = self.login("admin", "superadmin")
+        self.assertIn("token", response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- tees_hiring/urls.py
+++ tees_hiring/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    path(r'api-token-auth/', obtain_jwt_token),
-    path('', include('order.urls')),
+    path('admin/', admin.site.urls),
+    path(r'api-token-auth/', obtain_jwt_token, name="create-token"),
+    re_path('api/(?P<version>(v1|v2))/', include('order.urls'))
 ]
