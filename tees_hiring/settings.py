84fdc9ae694a7d8b2d2abff00482df27df3baa1e
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-12-16 10:30:59 +0700

    Setup JWT

--- tees_hiring/settings.py
+++ tees_hiring/settings.py
@@ -123,5 +123,13 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
--- tees_hiring/urls.py
+++ tees_hiring/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
+    path(r'api-token-auth/', obtain_jwt_token),
     path('', include('order.urls')),
 ]
